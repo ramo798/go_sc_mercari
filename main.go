4eaeede88bbc1c56811b5c511b41e5db6b2f2145
ramo798 <knht31i@gmail.com>
2021-05-10 12:29:27 +0900

    実行時間の計測

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"test/crawler"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	mercari_items := crawler.Get_items_on_mercari("951762445")
 	fmt.Println(mercari_items)
-
+	end := time.Now()
+	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
 }
