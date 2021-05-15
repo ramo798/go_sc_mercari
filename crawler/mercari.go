766e86416af4c7e716a1fb5e43200c38ef0df0cf
ramo798 <knht31i@gmail.com>
2021-05-16 01:19:09 +0900

    統合

--- crawler/mercari.go
+++ crawler/mercari.go
@@ -3,30 +3,27 @@ package crawler
 import (
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 	"test/model"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Get_items_url(urls []string) []string {
+func Get_items_url(url string) []string {
 	var resUrl []string
 
-	for _, url := range urls {
-		res, err := http.Get(url)
-		if err != nil {
-			log.Println(err)
-		}
-		defer res.Body.Close()
-
-		doc, _ := goquery.NewDocumentFromReader(res.Body)
-		doc.Find(".items-box a").Each(func(i int, s *goquery.Selection) {
-			url, _ := s.Attr("href")
-			resUrl = append(resUrl, "https://www.mercari.com"+url)
-			// fmt.Println(url)
-		})
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
 	}
+	defer res.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".items-box a").Each(func(i int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		resUrl = append(resUrl, "https://www.mercari.com"+url)
+		// fmt.Println(url)
+	})
 
 	return resUrl
 }
@@ -104,19 +101,44 @@ func Get_details_item(url string) model.Item_info_mercari {
 	return item_info
 }
 
+func process_m(url string, userid string, ch chan []model.Item_info_mercari) {
+	log.Println("process start", url)
+	var list []model.Item_info_mercari
+	items_url := Get_items_url(url)
+	for _, s := range items_url {
+		item_info := Get_details_item(s)
+		item_info.Username = userid
+		list = append(list, item_info)
+	}
+
+	ch <- list
+
+}
+
 func Get_items_on_mercari(userid string, username string) []model.Item_info_mercari {
 	var res []model.Item_info_mercari
 
 	url_list := Get_all_page_url(userid)
-	items_url := Get_items_url(url_list)
-	log.Println("Scraping START", "username:", username, "個数:", strconv.Itoa(len(items_url)))
 
-	for _, s := range items_url {
-		item_info := Get_details_item(s)
-		item_info.Username = username
-		log.Println(item_info)
-		res = append(res, item_info)
+	ch := make([]chan []model.Item_info_mercari, len(url_list))
+	for i, v := range url_list {
+		ch[i] = make(chan []model.Item_info_mercari)
+		go process_m(v, username, ch[i])
 	}
+
+	for i := range ch {
+		res = append(res, <-ch[i]...)
+		close(ch[i])
+	}
+	// items_url := Get_items_url(url_list)
+	// log.Println("Scraping START", "username:", username, "個数:", strconv.Itoa(len(items_url)))
+
+	// for _, s := range items_url {
+	// 	item_info := Get_details_item(s)
+	// 	item_info.Username = username
+	// 	log.Println(item_info)
+	// 	res = append(res, item_info)
+	// }
 
 	return res
 }
