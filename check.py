7d0ad6e55bd4ed1405a65e50b79f05fc6ab80752
ramo798 <knht31i@gmail.com>
2021-05-19 01:03:53 +0900

    check実装

---
+++ check.py
@@ -0,0 +1,35 @@
+import csv
+
+
+def p_no(p):
+    return p[p.find("[")+1:p.find("]")]
+
+
+inv = []
+
+ya = []
+
+me = []
+
+with open('Inventory_20210519.csv', "r", encoding="utf-8", errors="", newline="") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        inv.append(row)
+with open('mercari.csv', "r", encoding="utf-8", errors="", newline="") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        me.append(row)
+with open('yahuoku.csv', "r", encoding="utf-8", errors="", newline="") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        ya.append(row)
+
+
+for a in inv:
+    for b in ya:
+        if p_no(a[1]) == b[0]:
+            if int(a[5]) == 0:
+                print(b)
